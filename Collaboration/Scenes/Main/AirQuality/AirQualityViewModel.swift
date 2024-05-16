63ef7f84a02e031610b1677b8d271764e7bc6c55
Barbare Tepnadze <mestrolabe@Barbares-MacBook-Pro.local>
2024-05-17 01:09:23 +0400

    add network manager

--- Collaboration/Scenes/Main/AirQuality/AirQualityViewModel.swift
+++ Collaboration/Scenes/Main/AirQuality/AirQualityViewModel.swift
@@ -6,10 +6,13 @@
 //
 
 import Foundation
+import BarbareDoesNetworking
 
 class AirQualityViewModel {
     // MARK: - Selectors
-    
+    var catFactsCount = 0
+    var updateUI: (() -> Void)?
+    var showError: ((String) -> Void)?
     
     // MARK: - Variables
     
@@ -18,7 +21,32 @@ class AirQualityViewModel {
     
     
     // MARK: - Helper Functions
-    
+//    private let networkService = NetworkService()
+//       
+//       func fetchCatFacts() {
+//           guard let url = URL(string: "https://catfact.ninja/facts") else {
+//               showError?("Invalid URL")
+//               return
+//           }
+//           
+//           networkService.fetch(url: url, parse: { data in
+//               try? JSONDecoder().decode(CatFactsResponse.self, from: data)
+//           }) { (result: Result<CatFactsResponse?, NetworkError>) in
+//               DispatchQueue.main.async {
+//                   switch result {
+//                   case .success(let response):
+//                       if let facts = response?.data {
+//                           self.catFacts = facts
+//                           self.catFactsCount = self.catFacts.count
+//                           self.updateUI?()
+//                       }
+//                   case .failure(let error):
+//                       self.showError?("Error: \(error.localizedDescription)")
+//                   }
+//               }
+//           }
+//       }
+//    
     
     // MARK: - Computed Properties
     
