fdead85fdba67d33434588606e580accf50895a4
Elene Donadze <betterdays@Elenes-MacBook-Pro.local>
2024-05-18 00:21:47 +0400

    diax shevcvale

---
+++ Collaboration/Scenes/Main/Population/PopulationNetoworkService.swift
@@ -0,0 +1,31 @@
+//
+//  PopulationNetoworkService.swift
+//  Collaboration
+//
+//  Created by Elene Donadze on 5/17/24.
+//
+import Foundation
+import BarbareDoesNetworking
+
+class PopulationService {
+    
+    let networkService = NetworkService()
+    
+    func fetchPopulation(for country: String, completion: @escaping (Result<PopulationResponse?, NetworkError>) -> Void) {
+        guard let url = constructPopulationURL(for: country) else {
+            completion(.failure(.wrongResponse))
+            return
+        }
+        
+        networkService.fetch(url: url, parse: { data in
+            return try? JSONDecoder().decode(PopulationResponse.self, from: data)
+        }, completion: completion)
+    }
+    
+    private func constructPopulationURL(for country: String) -> URL? {
+        let encodedCountryName = country.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)
+        let urlString = "https://d6wn6bmjj722w.population.io:443/1.0/population/\(encodedCountryName ?? "")/today-and-tomorrow/"
+        return URL(string: urlString)
+    }
+}
+
--- Collaboration/Scenes/Main/Population/PopulationViewController.swift
+++ Collaboration/Scenes/Main/Population/PopulationViewController.swift
@@ -3,22 +3,26 @@
 //  Collaboration
 //
 //  Created by Barbare Tepnadze on 17.05.24.
-//
+
 import UIKit
 
 class PopulationViewController: UIViewController {
+    
     // MARK: - Variables
     var viewModel: PopulationViewModel
+    let populationService = PopulationService()
     
     // MARK: - UI Components
-    
+    var populationTextField = UITextField()
+    var populationButton = UIButton()
+    var todayLabel = UILabel()
+    var tomorrowLabel = UILabel()
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupLayout()
-        setupData()
-        view.backgroundColor = .blue
+        setupUI()
+        view.backgroundColor = .white
     }
     
     init(viewModel: PopulationViewModel) {
@@ -31,14 +35,82 @@ class PopulationViewController: UIViewController {
     }
     
     // MARK: - UI Setup
-    func setupLayout() {
+    func setupUI() {
+        // Configure populationTextField
+        populationTextField.placeholder = "Enter country name"
+        populationTextField.borderStyle = .roundedRect
+        populationTextField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(populationTextField)
+        
+        // Configure populationButton
+        populationButton.setTitle("Fetch Population", for: .normal)
+        populationButton.backgroundColor = .systemBlue
+        populationButton.setTitleColor(.white, for: .normal)
+        populationButton.layer.cornerRadius = 5
+        populationButton.addTarget(self, action: #selector(fetchPopulationData), for: .touchUpInside)
+        populationButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(populationButton)
+        
+        // Configure todayLabel
+        todayLabel.numberOfLines = 0
+        todayLabel.textAlignment = .center
+        todayLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(todayLabel)
+        
+        // Configure tomorrowLabel
+        tomorrowLabel.numberOfLines = 0
+        tomorrowLabel.textAlignment = .center
+        tomorrowLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tomorrowLabel)
+        
+        // Layout constraints
         NSLayoutConstraint.activate([
+            populationTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            populationTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 300),
+            populationTextField.widthAnchor.constraint(equalToConstant: 200),
+            populationTextField.heightAnchor.constraint(equalToConstant: 40),
             
+            populationButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            populationButton.topAnchor.constraint(equalTo: populationTextField.bottomAnchor, constant: 20),
+            populationButton.widthAnchor.constraint(equalToConstant: 150),
+            populationButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            todayLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            todayLabel.topAnchor.constraint(equalTo: populationButton.bottomAnchor, constant: 20),
+            todayLabel.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -40),
+            
+            tomorrowLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            tomorrowLabel.topAnchor.constraint(equalTo: todayLabel.bottomAnchor, constant: 20),
+            tomorrowLabel.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -40)
         ])
     }
     
-    // MARK: - Helper Functions
-    func setupData() {
+    // MARK: - Fetch Population Data
+    @objc func fetchPopulationData() {
+        guard let country = populationTextField.text, !country.isEmpty else {
+            todayLabel.text = "Please enter a country name."
+            tomorrowLabel.text = ""
+            return
+        }
         
+        populationService.fetchPopulation(for: country) { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let response):
+                    
+                    let todayDate = response?.total_population[0].date
+                    let tommorowDate = response?.total_population[1].date
+                    let todayPopulation = response?.total_population[0].population
+                    let tomorrowPopulation = response?.total_population[1].population
+                    
+                    self?.todayLabel.text = "\(String(describing: todayDate!))  \(String(describing: todayPopulation!))"
+                    self?.tomorrowLabel.text = "\(String(describing: tommorowDate!))  \(String(describing: tomorrowPopulation!))"
+                    
+                case .failure(let error):
+                    self?.todayLabel.text = "Error: \(error)"
+                    self?.tomorrowLabel.text = ""
+                }
+            }
+        }
     }
 }
