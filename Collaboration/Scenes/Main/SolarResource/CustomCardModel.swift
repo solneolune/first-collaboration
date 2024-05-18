e9318071cd1358bf10f4f8c82432d08f69f80542
Akaki <a.titberidze@yahoo.com>
2024-05-19 02:07:09 +0400

    Added solar files

---
+++ Collaboration/Scenes/Main/SolarResource/CustomCardModel.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCardModel.swift
+//  Collaboration
+//
+//  Created by Barbare Tepnadze on 17.05.24.
+//
+
+import Foundation
+
+struct CustomCard {
+    var icon: Icon
+    var title: String
+    var description: String
+}
